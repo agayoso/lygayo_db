00047046259a97c62c23dce5c4ee095c1877e46f
Andres Gayoso <a.gayoso@hotmail.com>
2024-12-27 07:30:03 -0300

    Actualizacion de parametros en PMT urbano

--- 06_PMT_Urbano.sql
+++ 06_PMT_Urbano.sql
@@ -7,27 +7,31 @@ BEGIN
 END
 GO
 
+-- Selección inicial de datos
 SELECT formulario,
-       v01b nro_dormitorios,
-       v03 tipo_piso,
-       v04 tipo_techo,
-       v12b tipo_combustible,
-       v1604 tiene_termocalefon,
-       v1609 tiene_auto_camion,
-       v1605 tiene_acondicionador_aire,
-       v1607 tiene_horno_microondas,
-       v1606 tiene_tv_cable,
-       v15b tiene_internet,
-       p09 edad,
-       p04 relacion_parentesco,
-       p05 es_miembro_hogar,
-       p11 estado_civil,
-       s01 tiene_seguro,
-       a02 trabajo_7dias,
-       a03 trabajo_1hr,
-       a04 trabajo_no_realizado,
-       b06 tipo_empleo,
-       ed01 idioma,
+       v01b AS nro_dormitorios,
+       v03 AS tipo_piso,
+       v04 AS tipo_techo,
+       v12b AS tipo_combustible,
+       v1604 AS tiene_termocalefon,
+       v1609 AS tiene_auto_camion,
+       v1605 AS tiene_acondicionador_aire,
+       v1607 AS tiene_horno_microondas,
+       v1606 AS tiene_tv_cable,
+       v15b AS tiene_internet,
+       v1603 AS tiene_cocina_elec,
+       v15a1 AS tiene_computadora,
+       v15a2 AS tiene_televisor,
+       p09 AS edad,
+       p04 AS relacion_parentesco,
+       p05 AS es_miembro_hogar,
+       p11 AS estado_civil,
+       s01 AS tiene_seguro,
+       a02 AS trabajo_7dias,
+       a03 AS trabajo_1hr,
+       a04 AS trabajo_no_realizado,
+       b06 AS tipo_empleo,
+       ed01 AS idioma,
        añoest,
        totpers,
        totmiem,
@@ -41,16 +45,10 @@ SELECT formulario,
        'VIVIENDA' AS categoria
 INTO #PMT_URBANO
 FROM RSH_BASE
-WHERE CASE
-           WHEN (dpto = 0 OR dpto = 11) AND area = 1 THEN 0
-           WHEN area = 6 THEN 6
-           WHEN area = 1 THEN 1
-       END = 1 AND
-       p05 = 1 AND
-       p04 < 11
+WHERE dominio = 1 AND p05 = 1 AND p04 < 11
 GO
 
--- Categorías de vivienda
+-- Agregar columnas para categorías de vivienda
 ALTER TABLE #PMT_URBANO
 ADD cat_agua_proveedor INT NULL,
     cat_banho_desague INT NULL,
@@ -79,7 +77,7 @@ UPDATE #PMT_URBANO
 SET cat_hogar_unipersonal = CASE WHEN tipohoga = 1 THEN 1 ELSE 0 END
 GO
 
--- Tabla temporal para jefe
+-- Crear tabla temporal para jefe
 IF OBJECT_ID('tempdb..#jefe_tabla') IS NOT NULL
 BEGIN
     DROP TABLE #jefe_tabla
@@ -99,7 +97,7 @@ FROM #PMT_URBANO
 WHERE relacion_parentesco = 1
 GO
 
--- Tabla temporal para hogar
+-- Crear tabla temporal para hogar
 IF OBJECT_ID('tempdb..#hogar_tabla') IS NOT NULL
 BEGIN
     DROP TABLE #hogar_tabla
@@ -118,7 +116,7 @@ FROM #PMT_URBANO
 GROUP BY formulario, area, dpto, dominio
 GO
 
--- Tabla final consolidada
+-- Crear tabla final consolidada
 IF OBJECT_ID('tempdb..#tabla_final') IS NOT NULL
 BEGIN
     DROP TABLE #tabla_final
@@ -129,20 +127,22 @@ SELECT pu.formulario, pu.area, pu.dpto, pu.dominio,
        pu.cat_agua_proveedor, pu.cat_banho_desague, pu.cat_basura, pu.cat_combustible, pu.cat_hogar_unipersonal,
        ht.hh_lndormitorios_pc, ht.hh_lntotpers, ht.hh_porc_0a14, ht.hh_porc_trabajan, ht.hh_porc_seguro,
        jt.jefe_anios_estudio, jt.jefe_cat_empleo, jt.jefe_cat_idioma,
-       pu.tiene_termocalefon, pu.tiene_auto_camion, pu.tiene_acondicionador_aire, pu.tiene_horno_microondas, pu.tiene_tv_cable, pu.tiene_internet
+       pu.tiene_termocalefon, pu.tiene_auto_camion, pu.tiene_acondicionador_aire, pu.tiene_horno_microondas,
+       pu.tiene_tv_cable, pu.tiene_internet, pu.tiene_cocina_elec, pu.tiene_computadora, pu.tiene_televisor
 INTO #tabla_final
 FROM #PMT_URBANO pu
 LEFT JOIN #hogar_tabla ht ON pu.formulario = ht.formulario AND pu.area = ht.area AND pu.dpto = ht.dpto AND pu.dominio = ht.dominio
 LEFT JOIN #jefe_tabla jt ON pu.formulario = jt.formulario AND pu.area = jt.area AND pu.dpto = jt.dpto AND pu.dominio = jt.dominio
 GO
 
--- Agregar columnas para cálculo de pobreza
+-- Agregar columnas para ingreso estimado y estatus de pobreza
 ALTER TABLE #tabla_final
 ADD lninc_est FLOAT NULL,
     status_pobreza INT NULL,
     status_pobreza_et VARCHAR(25) NULL
 GO
 
+-- Declarar constantes y ponderaciones
 DECLARE @w_cat_agua_proveedor FLOAT = 0.1007666
 DECLARE @w_cat_banho_desague FLOAT = 0.0521674
 DECLARE @w_cat_basura FLOAT = 0.0985621
@@ -162,10 +162,14 @@ DECLARE @w_tiene_horno_microondas FLOAT = 0.0906720
 DECLARE @w_tiene_internet FLOAT = 0.0718241
 DECLARE @w_tiene_tv_cable FLOAT = 0.0570942
 DECLARE @w_tiene_termocalefon FLOAT = 0.2819824
+DECLARE @w_tiene_cocina_elec FLOAT = 0.0424315
+DECLARE @w_tiene_computadora FLOAT = 0.1464662
+DECLARE @w_tiene_televisor FLOAT = 0.0933939
 DECLARE @constante FLOAT = 13.3317300
 DECLARE @lpobtot_urbano FLOAT = 13.62419
 DECLARE @lpobext_urbano FLOAT = 13.02462
 
+-- Calcular ingreso estimado
 UPDATE #tabla_final
 SET lninc_est = @constante +
     cat_agua_proveedor*@w_cat_agua_proveedor +
@@ -186,9 +190,13 @@ SET lninc_est = @constante +
     tiene_horno_microondas*@w_tiene_horno_microondas +
     tiene_internet*@w_tiene_internet +
     tiene_tv_cable*@w_tiene_tv_cable +
-    tiene_termocalefon*@w_tiene_termocalefon
+    tiene_termocalefon*@w_tiene_termocalefon +
+    tiene_cocina_elec*@w_tiene_cocina_elec +
+    tiene_computadora*@w_tiene_computadora +
+    tiene_televisor*@w_tiene_televisor
 GO
 
+-- Calcular estatus de pobreza
 UPDATE #tabla_final
 SET status_pobreza = CASE
                         WHEN lninc_est <= @lpobext_urbano THEN 1
@@ -200,7 +208,7 @@ SET status_pobreza = CASE
                             ELSE 'no pobre monetario' END
 GO
 
--- Generar tabla final
+-- Crear tabla final
 IF OBJECT_ID('PMT_URBANO') IS NOT NULL
 BEGIN
     DROP TABLE PMT_URBANO
